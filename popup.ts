c733b2422b728ad0702864a0536d29ae755b723d
David Timovski <david.timovski@gmail.com>
2019-02-10 18:36:44 +0100

    Streams source changed again

--- popup.ts
+++ popup.ts
@@ -2,71 +2,60 @@
 
 let httpClient = new HttpClient();
 
-httpClient.getJson('https://www.reddit.com/r/soccerstreams_pl', plResult => {
+httpClient.getJson('https://www.reddit.com/r/footballstreams_pl', plResult => {
 
   let posts = plResult.data.children;
 
-  httpClient.getJson('https://www.reddit.com/r/soccerstreams_other', otherResult => {
+  let app = new App();
+  let parser = new Parser();
+  let matches = parser.getMatchesFromPosts(posts);
+  
+  if (matches.length > 0) {
+    app.populateMatchesTable(matches);
+  } else {
+    DomHelper.showInfoMessage('There are currently no available matches.');
+  }
 
-    posts = posts.concat(otherResult.data.children);
+  DomHelper.hideElement(DomHelper.loadingGif);
 
-    let app = new App();
-    let parser = new Parser();
-    let matches = parser.getMatchesFromPosts(posts);
-    
-    if (matches.length > 0) {
-      app.populateMatchesTable(matches);
-    } else {
-      DomHelper.showInfoMessage('There are currently no available matches.');
+  DomHelper.showElement(DomHelper.matchesTable);
+
+  DomHelper.soccerStreamsLink.addEventListener('click', () => {
+    chrome.tabs.create({ url: 'https://www.reddit.com/r/footballstreams_pl', active: true });
+  }, false);
+  DomHelper.showElement(DomHelper.soccerStreamsLinkWrap);
+
+  let searchInput = <HTMLInputElement>document.getElementById('search');
+  searchInput.addEventListener('keyup', () => {
+    app.filterMatches(searchInput.value);
+  });
+  searchInput.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+      app.openFirstMatch();
     }
-  
-    DomHelper.hideElement(DomHelper.loadingGif);
-  
-    DomHelper.showElement(DomHelper.matchesTable);
-  
-    DomHelper.soccerStreamsPlLink.addEventListener('click', () => {
-      chrome.tabs.create({ url: 'https://www.reddit.com/r/soccerstreams_pl', active: true });
-    }, false);
-    DomHelper.soccerStreamsOtherLink.addEventListener('click', () => {
-      chrome.tabs.create({ url: 'https://www.reddit.com/r/soccerstreams_other', active: true });
-    }, false);
-    DomHelper.showElement(DomHelper.soccerStreamsLinkWrap);
-  
-    let searchInput = <HTMLInputElement>document.getElementById('search');
-    searchInput.addEventListener('keyup', () => {
-      app.filterMatches(searchInput.value);
-    });
-    searchInput.addEventListener('keyup', event => {
-      if (event.key === 'Enter') {
-        app.openFirstMatch();
-      }
-    });
-  
-    let body = document.getElementsByTagName('body')[0];
-    body.addEventListener('keyup', event => {
-      // If a letter was typed
-      let typingRegex: RegExp = /^[a-zA-Z]$/;
-      if (typingRegex.exec(event.key) !== null && DomHelper.matchesTable.style.display === 'block') {
-        if (searchInput.dataset.visible === 'false') {
-          searchInput.dataset.visible = 'true';
-          DomHelper.showElement(searchInput);
-          searchInput.focus();
-          searchInput.value = event.key;
-          app.filterMatches(searchInput.value);
-        }
-      } else if (event.key === 'Backspace') {
-        app.backToMatches();
-      }
-    });
+  });
 
-  }, () => { 
-    DomHelper.hideElement(DomHelper.loadingGif);
-  
-    DomHelper.showErrorMessage('An error occurred while loading the matches.'); 
+  let body = document.getElementsByTagName('body')[0];
+  body.addEventListener('keyup', event => {
+    // If a letter was typed
+    let typingRegex: RegExp = /^[a-zA-Z]$/;
+    if (typingRegex.exec(event.key) !== null && DomHelper.matchesTable.style.display === 'block') {
+      if (searchInput.dataset.visible === 'false') {
+        searchInput.dataset.visible = 'true';
+        DomHelper.showElement(searchInput);
+        searchInput.focus();
+        searchInput.value = event.key;
+        app.filterMatches(searchInput.value);
+      }
+    } else if (event.key === 'Backspace') {
+      app.backToMatches();
+    }
   });
 
 }, () => { 
   DomHelper.hideElement(DomHelper.loadingGif);
 
-  DomHelper.showErrorMessage('An error occurred while loading the matches.'); 
+  DomHelper.showElement(DomHelper.soccerStreamsNote);
+
+  DomHelper.showErrorMessage('An error occurred while loading the matches.');
 });
