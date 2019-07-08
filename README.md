a71309ed71cbe5dbf3bcd00083ecfe1913d7d7ed
David Timovski <david.timovski@gmail.com>
2019-07-09 00:24:53 +0200

    Streams source changed

--- README.md
+++ README.md
@@ -17,7 +17,8 @@ To all of the streamers and watchers at
 * ~~[www.reddit.com/r/footballconnection](https://www.reddit.com/r/footballconnection)~~
 * ~~[www.reddit.com/r/socceronreddit](https://www.reddit.com/r/socceronreddit)~~
 * ~~[www.reddit.com/r/InfluentialSpheres](https://www.reddit.com/r/InfluentialSpheres)~~
-* [www.reddit.com/r/redditsoccercity](https://www.reddit.com/r/redditsoccercity)
+* ~~[www.reddit.com/r/redditsoccercity](https://www.reddit.com/r/redditsoccercity)~~
+* [www.reddit.com/r/soccercom](https://www.reddit.com/r/soccercom)
 
 
 for making this extension possible.
--- popup.ts
+++ popup.ts
@@ -2,7 +2,7 @@
 
 let httpClient = new HttpClient();
 
-httpClient.getJson('https://www.reddit.com/r/redditsoccercity', result => {
+httpClient.getJson('https://www.reddit.com/r/soccercom', result => {
 
   let posts = result.data.children;
 
@@ -21,7 +21,7 @@ httpClient.getJson('https://www.reddit.com/r/redditsoccercity', result => {
   DomHelper.showElement(DomHelper.matchesTable);
 
   DomHelper.soccerStreamsLink.addEventListener('click', () => {
-    chrome.tabs.create({ url: 'https://www.reddit.com/r/redditsoccercity', active: true });
+    chrome.tabs.create({ url: 'https://www.reddit.com/r/soccercom', active: true });
   }, false);
   DomHelper.showElement(DomHelper.soccerStreamsLinkWrap);
 
